93de538fbb40bee06d1258cdac6abeb6aeaddf8b
Federico Tejedor Llorente <federicotllorente@gmail.com>
2024-03-26 21:22:08 +0100

    Start using GLEW and creating the first buffer

--- OpenGLTest/src/Application.cpp
+++ OpenGLTest/src/Application.cpp
@@ -1,13 +1,11 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    /* Initialize the GLFW library */
     if (!glfwInit())
         return -1;
 
@@ -22,8 +20,33 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Check if GLEW has been initialized properly */
     if (glewInit() != GLEW_OK)
         return -1;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f,
+    };
+
+    /* Create and bind a new buffer */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    /* Provide data to buffer */
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+        0, // index
+        2, // size
+        GL_FLOAT, // data type
+        GL_FALSE, // should it be normalized?
+        sizeof(float) * 2, // stride - the amount of memory we need to go to the next vertex (for more complex data structures we should use e.g. structs)
+        0 // pointer - the amount of memory we need to go to the next parameter (in this case, the next axis point)
+    );
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -31,11 +54,14 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        /*glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glEnd();*/
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
